9462b1e172f50733c781f0b61beaf7502015aa24
Roman Schigolev <r.shchigolev2018@yandex.ru>
2020-01-26 23:13:36 +0700

    continue learning js(objects)

--- objects.js
+++ objects.js
@@ -36,13 +36,30 @@ const CHARACTER = {
   name: 'John',
   age: 23,
   [city]: 'Moscow',
-  job, 
+  job,
   skill: 'do nothing',
   allKeys() {
     return Object.keys(this)
-                 .filter(item => item !== 'allKeys')
-                 .join(', ');
+      .filter(item => item !== 'allKeys')
+      .join(', ');
   }
-}
+};
 console.log(CHARACTER.allKeys()); // name, age, town, job, skill
 console.log(CHARACTER);
+
+const first = {a: 1};
+const second = {b: 2};
+console.log(Object.assign(first, second)); // { a: 1, b: 2 }
+console.log(first); // { a: 1, b: 2 }
+const third = {c: 3};
+console.log(Object.assign({}, third, second)); // { c: 3, b: 2 }. Создали новый массив, объединив два других
+console.log(third); // { c: 3 }
+const obj = {
+  one: 1,
+  two: 2,
+  three: 3
+};
+console.log(Object.keys(obj)); // [ 'one', 'two', 'three' ]
+console.log(Object.values(obj)); // [ 1, 2, 3 ]
+console.log(Object.entries(obj)); // [ [ 'one', 1 ], [ 'two', 2 ], [ 'three', 3 ]
+
